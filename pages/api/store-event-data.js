12b0df183bbe2130be3d0a227dd22b73168a7f1c
Otaiki1 <otaikisadiq2804@gmail.com>
2022-10-04 15:36:02 +0100

    refac: create storeEvent function

--- pages/api/store-event-data.js
+++ pages/api/store-event-data.js
@@ -13,3 +13,17 @@ export default async function handler(req, res){
             .json({message: "Method not allowed ", success: false})
     }
 }
+
+async function storeEventData(req, res){
+
+    const body  = req.body;
+    try{
+        const files = await makeFileObjects(body);
+        const cid = await storeFiles(files);
+        return res.status(200).json({success: true, cid: cid,})
+    }catch(err){
+        return res
+            .status(500)
+            .json({error: "Error Creating event", success: false});
+    }
+}
